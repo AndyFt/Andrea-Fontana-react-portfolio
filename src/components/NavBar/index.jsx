f3f5eb94458050212be8f1f6d5d73b29246dd303
AndyFt <149973636+AndyFt@users.noreply.github.com>
2024-02-18 21:21:48 +0000

    added react-router-dom, adjusted my navlinks, adjusted my project card to take in my props

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -11,8 +11,9 @@ function MyNav() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -1,17 +1,17 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-function ProjectCard() {
+function ProjectCard( { project } ) {
   return (
     <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>{project.title}</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          {project.description}
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <a href=''><Button variant="primary">Repo</Button></a>
+        <a href=''><Button variant="primary">Deployed site</Button></a>
       </Card.Body>
     </Card>
   );
