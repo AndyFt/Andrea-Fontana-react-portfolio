6d6e1301cc0c429adcacec84f57c8c6e963f3660
AndyFt <149973636+AndyFt@users.noreply.github.com>
2024-02-20 22:04:44 +0000

    added react-router-dom, adjusted my navlinks, adjusted my project card to take in my props, added gh-pages, added base to Router and vite configs, adjusted scripts in package.json, defined my output folder in vite.config, added my routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MyNav from './components/NavBar';
+import { Contact, Home, ProjectGallery } from './components/Pages';
 import './App.css'
 
 function App() {
 
   return (
-    <>
+    // Andrea-Fontana-react-portfolio
+    <Router basename='/'> 
       <MyNav />
-    </>
+      <Routes>
+      <Route path='/portfolio' element={<ProjectGallery />} />
+      <Route path='/contact' element={<Contact />} />
+      <Route path='' element={<Home />} />
+      </Routes>
+    </Router>
   )
 }
 
