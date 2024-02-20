52030bd44271f0db199b335d52dc7cc637a90897
AndyFt <149973636+AndyFt@users.noreply.github.com>
2024-02-20 22:51:40 +0000

    fixed the '.' issue

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -11,9 +11,9 @@ function MyNav() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="./">Home</Nav.Link>
-            <Nav.Link href="./portfolio">Portfolio</Nav.Link>
-            <Nav.Link href="./contact">Contact</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
