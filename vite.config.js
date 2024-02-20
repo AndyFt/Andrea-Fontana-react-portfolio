a71d52c37e52427db9d93312643e7828af6575a5
AndyFt <149973636+AndyFt@users.noreply.github.com>
2024-02-20 22:47:51 +0000

    added '.' on the navbar links to fix it

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Andrea-Fontana-react-portfolio',
+  base: './Andrea-Fontana-react-portfolio',
   build: {
     outDir: 'build'
   },
