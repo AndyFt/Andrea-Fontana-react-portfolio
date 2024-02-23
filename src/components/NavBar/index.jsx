ceda0e2bbd8b3d5a9825187d9bd58f9a744d16d2
AndyFt <149973636+AndyFt@users.noreply.github.com>
2024-02-23 17:29:43 +0000

    fixed navbar links position

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -10,7 +10,7 @@ function MyNav() {
         <Navbar.Brand href="./">Andrea Fontana</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto" style={{'flex-grow': '0'}}>
+          <Nav className="me-auto">
             <Nav.Link href="./">Home</Nav.Link>
             <Nav.Link href="./about">About Me</Nav.Link>
             <Nav.Link href="./portfolio">Portfolio</Nav.Link>
